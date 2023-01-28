4173e78c19ec98bfb166e179d488339e44c7d4f9
Vlad Bogush <bogush22092209@gmail.com>
2023-01-28 14:26:49 +0100

    q

--- Assets/ArCursor.cs
+++ Assets/ArCursor.cs
@@ -8,38 +8,98 @@ public class ArCursor : MonoBehaviour
     public GameObject cursorChildObject;
     public GameObject objectToPlace;
     public ARRaycastManager raycastManager;
-
-    public bool useCursor = true;
-
+    private GameObject objectUnderCursor = null;
     // Start is called before the first frame update
     void Start()
     {
-        cursorChildObject.SetActive(useCursor);
+        cursorChildObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (useCursor)
+        UpdateCursor();
+        checkInput();
+    }
+
+    void checkInput()
+    {
+        if (objectUnderCursor)
         {
-            UpdateCursor();
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            Debug.Log($"objectUnderCursor: {objectUnderCursor}");
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary)
             {
-                GameObject.Instantiate(objectToPlace, transform.position, transform.rotation);
+                
+                var newPos = Camera.main.transform.position + (Camera.main.transform.forward * 0.5f);
+                objectUnderCursor.transform.position = newPos;
+                objectUnderCursor.transform.rotation = Camera.main.transform.rotation;
             }
         }
+        else
+        {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Debug.Log($"objectUnderCursor: {objectUnderCursor}");
+                if (objectUnderCursor)
+                {
+                    var renderer = objectUnderCursor.GetComponent<Renderer>();
+                    renderer.material.SetColor("_Color", Color.red);
+                }
+                else
+                {
+                    var objectPos = new Vector3(transform.position.x, Camera.main.transform.position.y, transform.position.z);
+                    GameObject.Instantiate(objectToPlace, objectPos, transform.rotation);
+                }
+
+            }
+        }
+
+        
 
         
     }
 
     void UpdateCursor()
     {
+
+        if (updateScreenHit())
+        {
+            return;
+        }
+
+        updatePlaneHit();
+
+
+    }
+
+    bool updateScreenHit()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 5.0f))
+        {
+            if (hit.collider.name == "Screen(Clone)")
+            {
+                objectUnderCursor = hit.collider.gameObject;
+                transform.position = hit.point + (hit.normal * 0.001f);
+                transform.rotation = Quaternion.LookRotation(hit.normal);
+                transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x - 90, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+                return true;
+            }
+        }
+        objectUnderCursor = null;
+        return false;
+    }
+
+    void updatePlaneHit()
+    {
         Vector2 screenPosition = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
         raycastManager.Raycast(screenPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
-        
+
         if (hits.Count > 0)
-        {
+        {           
             transform.position = hits[0].pose.position;
             transform.rotation = hits[0].pose.rotation;
         }
